8e68d0e1060bbf9b811e60bedbee21945fb68b68
ksj1230 <38639076+ksj1230@users.noreply.github.com>
2018-04-25 00:21:47 +0900

    Update pc_detect.ps1
    Add a simple description

--- pc_detect.ps1
+++ pc_detect.ps1
@@ -11,13 +11,27 @@
    This software has dual license (MIT and GPL v2). See the GPL_LICENSE and
    MIT_LICENSE file.
 #>
- 
-$SideBySide = Get-ChildItem -Path 'Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\SideBySide\Winners\'
 
-#Get the latest component versions and its corresponding packages
+<#
+Download this script, right click and click Run with Powershell.
+
+Simple Descrption
+1. List the installed packages on the system
+ : We assume that the ComponentDetect registry has not been tampered with by an attacker.
+ : We analyze the ComponentDetect registry information to identify the updated components by package installation
+   and the latest version of those components.
+2. Check the package-component mappings
+ : We verify that the component version in the SideBySide registry is correct based on the latest component version
+   information identified in step 1.
+3. Verify the hardlink information of components
+#>
+
+Write-Host -NoNewLine 'Now Scanning...';
+
+#1. List the installed packages on the system
 $ComponentDetect = Get-ChildItem -Path 'Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\ComponentDetect\'
-
 $DictAnswer = @{} #nested dictionary {component_name, {package_name, component_version}}
+
 ForEach($i in 0..($ComponentDetect.Length-1)) {
     $RegPath = "Registry::"
     $RegPath += $ComponentDetect.Item($i).Name
@@ -25,12 +39,9 @@ ForEach($i in 0..($ComponentDetect.Length-1)) {
     $DictPackage = @{}
     ForEach($j in 0..($Info.psobject.Properties.Name.Length-1)){
         If ($Info.psobject.Properties.Name.Item($j).tostring() -match "Package_"){
-            #$Info.psobject.Properties.Name.Item($j)
-            #$Info.psobject.Properties.Value.Item($j)
             $DictPackage.Add($Info.psobject.Properties.Name.Item($j), $Info.psobject.Properties.Value.Item($j).Split("@")[0])
         }
     }
-    #Sort the dictionary (by key, descending order)
     $DictPackage = $DictPackage.GetEnumerator() | sort -Property value -Descending
 
     $temp_key = $RegPath.Split("\\")[($RegPath.Split("\\").Length)-1]
@@ -42,6 +53,8 @@ ForEach($i in 0..($ComponentDetect.Length-1)) {
     }
 }
 
+#2. Check the package-component mappings
+$SideBySide = Get-ChildItem -Path 'Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\SideBySide\Winners\'
 ForEach($i in 0..(($SideBySide.Length)-1) ){
     $NameInSxS = $SideBySide.Item($i).Name.Split("\\")[($SideBySide.Item($i).Name.Split("\\").Length)-1]
     $temp_name = $NameInSxS.Split("_")[0] + "_" + $NameInSxS.Split("_")[1] + "_" + $NameInSxS.Split("_")[($NameInSxS.Split("_").Length) - 2]
@@ -54,8 +67,6 @@ ForEach($i in 0..(($SideBySide.Length)-1) ){
         }
         Catch{
         }
-        #$Check
-        #$Check.psobject.Properties.Name.Length
         If(6 -ge $Check.psobject.Properties.Name.Length){continue}
         
         #Find the Correct Version
@@ -79,5 +90,6 @@ ForEach($i in 0..(($SideBySide.Length)-1) ){
     }
 }
 
+#When running in ISE, annotating the following codes
 Write-Host -NoNewLine 'Press any key to exit...';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
